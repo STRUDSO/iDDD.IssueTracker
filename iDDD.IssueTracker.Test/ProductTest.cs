a84fbcacd0f53f2be760ee5d2dbf8ca8addbd69d
Søren Trudsø Mahon <strudsomahon@ebay.com>
2013-05-13 16:51:08 +0200

    test project

---
+++ iDDD.IssueTracker.Test/ProductTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace iDDD.IssueTracker.Test
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void Fact()
+        {
+            
+        }
+        [Theory, AutoMoqData]
+        public void Foo([Frozen]Mock<IDisposable> disposable, asdasd bla)
+        {
+            disposable.Verify(x => x.Dispose());
+        }
+    }
+    public class asdasd
+    {
+        public asdasd(IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
