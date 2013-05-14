0e50a68582eee72c363873bc302dd9219e6bdc69
Søren Trudsø Mahon <strudsomahon@ebay.com>
2013-05-14 09:53:42 +0200

    productid

---
+++ iDDD.IssueTracker.Domain/Products/ProductId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SaasOvation.IssueTrack.Domain.Model.Products
+{
+    public class ProductId
+    {
+        public ProductId()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; private set; }
+    }
+}
---
+++ iDDD.IssueTracker.Test/Domain/Model/Products/ProductIdTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ploeh.AutoFixture;
+using SaasOvation.IssueTrack.Domain.Model.Products;
+using Xunit;
+using Xunit.Extensions;
+
+namespace iDDD.IssueTracker.Test.Domain.Products
+{
+    public class ProductIdTests
+    {
+        [Fact]
+        public void Dummy(){}
+
+        [Theory, AutoMoqData]
+        public void New_Can_CreateProductId(Generator<ProductId> productIds)
+        {
+            IEnumerable<ProductId> enumerable = productIds.Take(2);
+
+            Assert.NotEqual(enumerable.First(), enumerable.Last());
+        }
+
+        [Theory, AutoMoqData]
+        public void New_DefaultCtor_GeneratesUniqueId(ProductId productId)
+        {
+            Assert.NotNull(productId.Id);
+        }
+    }
+}
